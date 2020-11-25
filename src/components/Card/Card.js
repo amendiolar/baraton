ee455ec18dfa8280e92111827415c140c73934ce
amendiolar <abel.mendiola@comunidad.unam.mx>
2020-11-24 21:30:12 -0600

    Cards

---
+++ src/components/Card/Card.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+function Card (){
+
+    return(
+    <div className="card-deck">
+        <div className="card">
+            <img src="https://i.pinimg.com/originals/eb/83/be/eb83be580847bcdc4c8f403c8085d3c8.jpg" className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">
+                    Card title (product_name)
+                </h5>
+                <p className="card-text">
+                    (description) Some quick example text to build on the card title and make up the bulk of the  content.
+                </p>
+             </div>
+            <ul className="list-group list-group-flush">
+                <li className="list-group-item">(brand)Cras justo odio</li>
+                <li className="list-group-item">(price)Dapibus ac facilisis in</li>
+            </ul>
+            <div className="card-body">
+                <button type="button" className="btn btn-success">Agregar</button>
+            </div>
+        </div>
+        <div className="card">
+            <img src="https://i.pinimg.com/originals/eb/83/be/eb83be580847bcdc4c8f403c8085d3c8.jpg" className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">
+                    Card title (product_name)
+                </h5>
+                <p className="card-text">
+                    (description) Some quick example text to build on the card title and make up the bulk of the  content.
+                </p>
+             </div>
+            <ul className="list-group list-group-flush">
+                <li className="list-group-item">(brand)Cras justo odio</li>
+                <li className="list-group-item">(price)Dapibus ac facilisis in</li>
+            </ul>
+            <div className="card-body">
+                <button type="button" className="btn btn-success">Agregar</button>
+            </div>
+        </div>
+        <div className="card">
+            <img src="https://i.pinimg.com/originals/eb/83/be/eb83be580847bcdc4c8f403c8085d3c8.jpg" className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">
+                    Card title (product_name)
+                </h5>
+                <p className="card-text">
+                    (description) Some quick example text to build on the card title and make up the bulk of the  content.
+                </p>
+             </div>
+            <ul className="list-group list-group-flush">
+                <li className="list-group-item">(brand)Cras justo odio</li>
+                <li className="list-group-item">(price)Dapibus ac facilisis in</li>
+            </ul>
+            <div className="card-body">
+                <button type="button" className="btn btn-success">Agregar</button>
+            </div>
+        </div>
+    </div>    
+    );
+
+}
+
+export default Card;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import Card from '../components/Card';
 function Home (){
     return(
         <div>
             <Navbar></Navbar>
             <h1>Bienvenido a la tienda del Baraton</h1>
+            <Card></Card>
         </div>
     )
 }
