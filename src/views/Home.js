eaf9d5379f90e1bf498f81b7f4dd2b580cd07ea6
amendiolar <abel.mendiola@comunidad.unam.mx>
2020-11-24 20:00:46 -0600

    Navbar

---
+++ src/views/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+function Home (){
+    return(
+        <div>
+            <Navbar></Navbar>
+            <h1>Bienvenido a la tienda del Baraton</h1>
+        </div>
+    )
+}
+export default Home;
